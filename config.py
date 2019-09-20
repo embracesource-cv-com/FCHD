94038823248ec89784417b3d6b79980fcf3309b4
ybcc <271380178@qq.com>
2019-09-20 22:07:15 +0800

    add config of caffe pretrained model

--- config.py
+++ config.py
@@ -4,6 +4,8 @@
 class Config(object):
     DATASET_DIR = r'G:\fchd-dataset\brainwash'
     ANNOTS_FILE = 'brainwash_train.idl'
+    CAFFE_PRETRAIN = True
+    CAFFE_PRETRAIN_PATH = ''
 
 
 cfg = Config()
