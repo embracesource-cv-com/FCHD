33f2782b5f8ec904911848d0a386bd69785966e7
ybcc <271380178@qq.com>
2019-10-08 14:45:49 +0800

    add RPN configuration

--- config.py
+++ config.py
@@ -7,5 +7,12 @@ class Config(object):
     CAFFE_PRETRAIN = True
     CAFFE_PRETRAIN_PATH = ''
 
+    RPN_NMS_THRESH = 0.7,
+    RPN_TRAIN_PRE_NMS_TOP_N = 12000,
+    RPN_TRAIN_POST_NMS_TOP_N = 300,
+    RPN_TEST_PRE_NMS_TOP_N = 6000,
+    RPN_TEST_POST_NMS_TOP_N = 300,
+    RPN_MIN_SIZE = 16
+
 
 cfg = Config()
