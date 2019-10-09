327f5b2d8b04fcb542dc0760e72dd97caa11e6e0
ybcc <271380178@qq.com>
2019-10-09 10:58:33 +0800

    implement call() method of ProposalCreator

--- networks/rpn.py
+++ networks/rpn.py
@@ -3,6 +3,7 @@ from torch import nn
 import numpy as np
 from utils.tools import generate_anchors
 from ..config import cfg
+from utils import tools
 
 
 class RPN(nn.Module):
@@ -65,4 +66,31 @@ class ProposalCreator(object):
         self.nms_thresh = cfg.RPN_NMS_THRESH
         self.min_size = cfg.RPN_MIN_SIZE
 
-    
+    def __call__(self, deltas, scores, anchors, img_size, scale):
+        if self.parent_model.training:
+            pre_nms_top_N = cfg.RPN_TRAIN_PRE_NMS_TOP_N
+            post_nms_top_N = cfg.RPN_TRAIN_POST_NMS_TOP_N
+        else:
+            pre_nms_top_N = cfg.RPN_TEST_PRE_NMS_TOP_N
+            post_nms_top_N = cfg.RPN_TEST_POST_NMS_TOP_N
+
+        proposals = tools.bbox_regression(anchors, deltas)
+        proposals = tools.clip_boxes(proposals, img_size)
+
+        keep = tools.filter_boxes(proposals, self.min_size * scale)
+        proposals = proposals[keep, :]
+        scores = scores[keep]
+
+        order = scores.argsort()[::-1]
+        if pre_nms_top_N > 0:
+            order = order[:pre_nms_top_N]
+        proposals = proposals[order, :]
+        scores = scores[order]
+
+        keep = tools.nms(proposals, self.nms_thresh)
+        if post_nms_top_N > 0:
+            keep = keep[:post_nms_top_N]
+        proposals = proposals[keep, :]
+        scores = scores[keep]
+
+        return proposals, scores
