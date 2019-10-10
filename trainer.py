700cddf5b3cdf4ac0b28653ca59c9ebdb5ea5b8a
ybcc <271380178@qq.com>
2019-10-10 11:31:03 +0800

    create Trainer cls

---
+++ trainer.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+class Trainer(nn.Module):
+    def __init__(self):
+        super(Trainer, self).__init__()
+        pass
