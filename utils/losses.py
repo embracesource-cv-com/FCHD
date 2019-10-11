90a5ebf4c6cbc27c119ac020fed562a0524616e6
ybcc <271380178@qq.com>
2019-10-11 16:57:36 +0800

    rpn regression loss

---
+++ utils/losses.py
@@ -0,0 +1,18 @@
+import torch
+
+
+def rpn_regr_loss(pred_rger, gt_rger, gt_labels):
+    weights = torch.zeros(gt_rger.size()).cuda()
+    weights[(gt_labels > 0).view(-1, 1).expand_as(weights).cuda()] = 1
+    loss = smooth_l1_loss(pred_rger, gt_rger, weights)
+    loss /= (gt_labels >= 0).sum()  # ignore gt_labels == -1
+    return loss
+
+
+def smooth_l1_loss(preds, gts, weights, sigma=3):
+    sigma2 = sigma ** 2
+    diff = (preds - gts) * weights
+    abs_diff = diff.abs()
+    flag = (abs_diff < (1 / sigma2)).float()
+    loss = flag * (sigma2 / 2) * (diff ** 2) + (1 - flag) * (abs_diff - 0.5 / sigma2)
+    return loss.sum()
