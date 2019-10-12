f5651e968877d3a0b21e6da1cc156ec4bc7d876d
ybcc <271380178@qq.com>
2019-10-12 13:54:56 +0800

    update

--- utils/visualize.py
+++ utils/visualize.py
@@ -4,6 +4,7 @@ import cv2
 import visdom
 import time
 import torch
+from matplotlib import pyplot as plot
 
 
 def draw_box(img, box, color='red', thickness=4, use_normalized_coordinates=False):
@@ -128,3 +129,81 @@ class Visualizer(object):
         self.log_text = d.get('log_text', '')
         self.index = d.get('index', dict())
         return self
+
+
+def vis_image(img, ax=None):
+    """Visualize a color image.
+    Args:
+        img (~numpy.ndarray): An array of shape :math:`(3, height, width)`.
+            This is in RGB format and the range of its value is
+            :math:`[0, 255]`.
+        ax (matplotlib.axes.Axis): The visualization is displayed on this
+            axis. If this is :obj:`None` (default), a new axis is created.
+
+    Returns:
+        ~matploblib.axes.Axes:
+        Returns the Axes object with the plot for further tweaking.
+    """
+    if ax is None:
+        fig = plot.figure()
+        ax = fig.add_subplot(1, 1, 1)
+    # CHW -> HWC
+    img = img.transpose((1, 2, 0))
+    ax.imshow(img.astype(np.uint8))
+    return ax
+
+
+def vis_bbox(img, bbox):
+    # Returns newly instantiated matplotlib.axes.Axes object if ax is None
+    ax = vis_image(img, ax=None)
+
+    # If there is no bounding box to display, visualize the image and exit.
+    if len(bbox) == 0:
+        return ax
+
+    for i, bb in enumerate(bbox):
+        xy = (bb[1], bb[0])
+        height = bb[2] - bb[0]
+        width = bb[3] - bb[1]
+        ax.add_patch(plot.Rectangle(
+            xy, width, height, fill=False, edgecolor='red', linewidth=2))
+
+    return ax
+
+
+def fig2data(fig):
+    """
+    brief Convert a Matplotlib figure to a 4D numpy array with RGBA
+    channels and return it
+
+    @param fig： a matplotlib figure
+    @return a numpy 3D array of RGBA values
+    """
+    # draw the renderer
+    fig.canvas.draw()
+
+    # Get the RGBA buffer from the figure
+    w, h = fig.canvas.get_width_height()
+    buf = np.fromstring(fig.canvas.tostring_argb(), dtype=np.uint8)
+    buf.shape = (w, h, 4)
+
+    # canvas.tostring_argb give pixmap in ARGB mode. Roll the ALPHA channel to have it in RGBA mode
+    buf = np.roll(buf, 3, axis=2)
+    return buf.reshape(h, w, 4)
+
+
+def fig4vis(fig):
+    """
+    convert figure to ndarray
+    """
+    ax = fig.get_figure()
+    img_data = fig2data(ax).astype(np.int32)
+    plot.close()
+    # HWC->CHW
+    return img_data[:, :, :3].transpose((2, 0, 1)) / 255.
+
+
+def visdom_bbox(img, rois):
+    fig = vis_bbox(img, rois)
+    data = fig4vis(fig)
+    return data
