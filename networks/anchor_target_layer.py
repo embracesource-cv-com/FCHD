8fcfc06f42ea2252a261191935e379eb6b309730
ybcc <271380178@qq.com>
2019-10-14 16:17:46 +0800

    some fix in _unmap() method

--- networks/anchor_target_layer.py
+++ networks/anchor_target_layer.py
@@ -60,11 +60,11 @@ class AnchorTargetLayer(object):
         """ Unmap a subset of item (data) back to the original set of items (of
         size count) """
         if len(data.shape) == 1:
-            ret = np.empty((count,), dtype=np.float32)
+            ret = np.empty((count,), dtype=data.dtype)
             ret.fill(fill)
             ret[inds] = data
         else:
-            ret = np.empty((count,) + data.shape[1:], dtype=np.float32)
+            ret = np.empty((count,) + data.shape[1:], dtype=data.dtype)
             ret.fill(fill)
             ret[inds, :] = data
         return ret
