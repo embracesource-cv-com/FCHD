0a4a91f64bf60593f9e6b53c5560ce0e84775561
ybcc <271380178@qq.com>
2019-10-14 12:57:03 +0800

    fix some bug

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ class Config(object):
     CAFFE_PRETRAIN = True
     CAFFE_PRETRAIN_PATH = './checkpoints/pre_trained/vgg16_caffe.pth'
 
-    RPN_NMS_THRESH = 0.7,
-    RPN_TRAIN_PRE_NMS_TOP_N = 12000,
-    RPN_TRAIN_POST_NMS_TOP_N = 300,
-    RPN_TEST_PRE_NMS_TOP_N = 6000,
-    RPN_TEST_POST_NMS_TOP_N = 300,
+    RPN_NMS_THRESH = 0.7
+    RPN_TRAIN_PRE_NMS_TOP_N = 12000
+    RPN_TRAIN_POST_NMS_TOP_N = 300
+    RPN_TEST_PRE_NMS_TOP_N = 6000
+    RPN_TEST_POST_NMS_TOP_N = 300
     RPN_MIN_SIZE = 16
 
     ANCHOR_RATIOS = [1]
@@ -21,6 +21,7 @@ class Config(object):
 
     EPOCHS = 15
     DEBUG = True
+    PRINT_LOG = True
     PLOT_INTERVAL = 2
     VISDOM_ENV = 'fchd'
     MODEL_DIR = './checkpoints'
