f53f7dac1f59fab76c067f852bc824d1a6f603eb
ybcc <271380178@qq.com>
2019-10-14 11:12:09 +0800

    update

--- config.py
+++ config.py
@@ -2,12 +2,12 @@
 
 
 class Config(object):
-    DATASET_DIR = r'G:\fchd-dataset\brainwash'
+    DATASET_DIR = '/home/dataset/fchd_datas/brainwash'
     TRAIN_ANNOTS_FILE = 'brainwash_train.idl'
     VAL_ANNOTS_FILE = 'brainwash_val.idl'
 
     CAFFE_PRETRAIN = True
-    CAFFE_PRETRAIN_PATH = ''
+    CAFFE_PRETRAIN_PATH = './checkpoints/pre_trained/vgg16_caffe.pth'
 
     RPN_NMS_THRESH = 0.7,
     RPN_TRAIN_PRE_NMS_TOP_N = 12000,
@@ -22,7 +22,7 @@ class Config(object):
     EPOCHS = 15
     DEBUG = True
     PLOT_INTERVAL = 2
-    VISDOM_ENV = 'head_detector'
+    VISDOM_ENV = 'fchd'
     MODEL_DIR = './checkpoints'
 
 
