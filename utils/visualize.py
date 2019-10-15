29b4904fee5d62e09fd40637918872155609c4e4
ybcc <271380178@qq.com>
2019-10-15 20:39:32 +0800

    Visualize the origin & pre-processed image

--- utils/visualize.py
+++ utils/visualize.py
@@ -1,5 +1,4 @@
 import numpy as np
-from PIL import Image, ImageDraw
 import cv2
 import visdom
 import time
@@ -7,47 +6,40 @@ import torch
 from matplotlib import pyplot as plot
 
 
-def draw_box(img, box, color='red', thickness=4, use_normalized_coordinates=False):
-    img_pil = Image.fromarray(img).convert('RGB')
-    _draw(img_pil, box, color, thickness, use_normalized_coordinates)
-    np.copyto(img, np.array(img_pil))
+def show_origin_img(data):
+    """
+    Visualize the original image
+    """
+    path = data['img_path']
+    boxes = data['boxes']
 
-
-def _draw(img, box, color, thickness, use_normalized_coordinates):
-    ymin, xmin, ymax, xmax = box
-    draw = ImageDraw.Draw(img)
-    im_width, im_height = img.size
-    if use_normalized_coordinates:
-        left, right, top, bottom = xmin * im_width, xmax * im_width, ymin * im_height, ymax * im_height
-    else:
-        left, right, top, bottom = xmin, xmax, ymin, ymax
-    draw.line([(left, top), (left, bottom), (right, bottom),
-              (right, top), (left, top)], width=thickness, fill=color)
-
-
-def check_raw_data(data_info):
-    path = data_info['img_path']
-    counts = data_info['counts']
-    boxes = data_info['boxes']
     img = cv2.imread(path)
-    img_cp = np.copy(np.asarray(img, dtype=np.uint8))
-    for i in range(counts):
-        draw_box(img_cp, boxes[i])
+
+    for box in boxes.astype(np.int):
+        ymin, xmin, ymax, xmax = box
+        cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 0, 255), 2)
+
+    cv2.imshow('image', img)
+    cv2.waitKey(3000)
+    cv2.destroyAllWindows()
+    # cv2.imwrite('raw_img.png', img)
+
+
+def show_processed_img(img, boxes):
+    """
+    Visualize the pre-processed image
+    """
+    img = img.transpose((1, 2, 0))  # (C,H,W) -> (H,W,C)
+    img_cp = img.copy()
+
+    for box in boxes.astype(np.int):
+        ymin, xmin, ymax, xmax = box
+        cv2.rectangle(img_cp, (xmin, ymin), (xmax, ymax), (0, 0, 255), 2)
 
     cv2.imshow('image', img_cp)
     cv2.waitKey(3000)
     cv2.destroyAllWindows()
-
-
-def check_transformed_data(img, boxes):
-    img = img.transpose((1, 2, 0))
-    img_cp = np.copy(np.asarray(img, dtype=np.uint8))
-    for i in range(len(boxes)):
-        draw_box(img_cp, boxes[i])
-
-    cv2.imshow('image', img_cp)
-    cv2.waitKey(3000)
-    cv2.destroyAllWindows()
+    # cv2.imwrite('precessed_img.png', img_cp)
 
 
 class Visualizer(object):
