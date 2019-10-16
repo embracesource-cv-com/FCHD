129c2ffed23805d40fe6774c22bfab28df545634
ybcc <271380178@qq.com>
2019-10-16 16:12:25 +0800

    add comments

--- networks/proposal_layer.py
+++ networks/proposal_layer.py
@@ -10,6 +10,15 @@ class ProposalLayer(object):
         self.min_size = cfg.RPN_MIN_SIZE
 
     def __call__(self, deltas, scores, anchors, img_size, scale):
+        """
+
+        :param deltas: 2d array, shape(n, 4)
+        :param scores: 1d array, shape(n, )
+        :param anchors: 2d array, shape(n, 4)
+        :param img_size: tuple, (h, w)
+        :param scale: float
+        :return:
+        """
         if self.parent_model.training:
             pre_nms_top_N = cfg.RPN_TRAIN_PRE_NMS_TOP_N
             post_nms_top_N = cfg.RPN_TRAIN_POST_NMS_TOP_N
@@ -17,19 +26,25 @@ class ProposalLayer(object):
             pre_nms_top_N = cfg.RPN_TEST_PRE_NMS_TOP_N
             post_nms_top_N = cfg.RPN_TEST_POST_NMS_TOP_N
 
+        # bounding boxes regression with deltas
         proposals = tools.bbox_regression(anchors, deltas)
+
+        # clip boxes into image boundaries
         proposals = tools.clip_boxes(proposals, img_size)
 
+        # remove all boxes with any side smaller than min_size.
         keep = tools.filter_boxes(proposals, self.min_size * scale)
         proposals = proposals[keep, :]
         scores = scores[keep]
 
+        # sort boxes in descending order by score
         order = scores.argsort()[::-1]
         if pre_nms_top_N > 0:
             order = order[:pre_nms_top_N]
         proposals = proposals[order, :]
         scores = scores[order]
 
+        # nms
         keep = tools.nms(np.hstack((proposals, scores.reshape(-1, 1))), self.nms_thresh)
         if post_nms_top_N > 0:
             keep = keep[:post_nms_top_N]
